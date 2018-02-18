6aacb883ed6a21083c5cd7db6d88b61abf78bdd9
Laura Ramos <lauramilena.rbe@gmail.com>
2018-02-18 17:45:44 -0500

    Profile configuration initial

---
+++ src/app/pages/profile-config-page/profile-config-page.component.ts
@@ -0,0 +1,21 @@
+import{Component, OnInit}from '@angular/core';
+import {Router}from '@angular/router';
+import {UserService}from '../../services/user.service';
+import {User} from '../../models/user';
+
+@Component({
+  selector: 'app-profile-config-page',
+  templateUrl: './profile-config-page.component.html',
+  styleUrls: ['./profile-config-page.component.css']
+})
+export class ProfileConfigPageComponent implements OnInit {
+
+
+  constructor() {
+
+  }
+  ngOnInit() {
+
+  }
+
+}
